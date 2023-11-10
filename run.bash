dc4ec0c434c20d669988121da3c42e181bffc52c
Enric Cervera <ecervera@uji.es>
2023-11-10 11:15:14 +0100

    Change bag folder

--- run.bash
+++ run.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-BAGS_FOLDER="${HOME}/Escriptori/IR2121/SLAM_TB3/"
+BAGS_FOLDER="${HOME}/Escriptori/IR2121/SLAM_TB3/bags"
 
 docker run --rm -it -p 8888:8888 -v ${BAGS_FOLDER}:/home/jovyan/bags nml_bag:latest
 
